4084864fc273d0a8ef7a62bdda4ba636664025d2
exrol <exrol@protonmail.com>
2022-10-01 02:23:14 +0200

    refactor(DarkMode): single button version

--- js/main.js
+++ js/main.js
@@ -1,12 +1,44 @@
-function darkMode() {
+//Buttons
+const lightModeButton = document.getElementById("light-mode-button");
+const darkModeButton = document.getElementById("dark-mode-button");
+
+//Additional params
+lightModeButton.theme = 'light'
+darkModeButton.theme = 'dark'
+
+hideButton(darkModeButton)
+
+const setMode = (event) => {
     var element = document.body;
-    var content = document.getElementById("DarkModetext");
-    element.className = "dark-mode";
-    content.innerText = "Dark Mode is ON";
+    const theme = event.currentTarget.theme
+
+    if (theme === 'dark') {
+        var content = document.getElementById("DarkModetext");
+        element.className = "dark-mode";
+        content.innerText = "Dark Mode is ON";
+        hideButton(darkModeButton)
+        showButton(lightModeButton)
+    } else {
+        var content = document.getElementById("DarkModetext");
+        element.className = "light-mode";
+        content.innerText = "Dark Mode is OFF";
+        showButton(darkModeButton)
+        hideButton(lightModeButton)
+    }
 }
-function lightMode() {
-    var element = document.body;
-    var content = document.getElementById("DarkModetext");
-    element.className = "light-mode";
-    content.innerText = "Dark Mode is OFF";
+
+//Event listener
+lightModeButton.addEventListener("click", setMode);
+darkModeButton.addEventListener("click", setMode);
+
+
+
+
+function hideButton(button) {
+    button.style.display = "none";
+}
+
+
+function showButton(button) {
+    button.style.display = "block";
 }
